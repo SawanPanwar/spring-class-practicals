9e3442ccd9800a3f380ef76b5ef1e045f07f07d5
SawanPanwar <skpanwar25@gmail.com>
2025-05-23 15:54:50 +0530

    23-05-2025

--- spring-boot-simple/src/main/java/com/rays/service/UserService.java
+++ spring-boot-simple/src/main/java/com/rays/service/UserService.java
@@ -1,5 +1,7 @@
 package com.rays.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -41,5 +43,39 @@ public class UserService {
 		UserDTO dto = dao.findByPk(pk);
 		return dto;
 	}
+	
+	@Transactional(readOnly = true)
+	public List search(UserDTO dto, int pageNo, int pageSize) {
+		List list = dao.search(dto, pageNo, pageSize);
+		return list;
+	}
 
+	@Transactional(readOnly = true)
+	public UserDTO findByLogin(String login) {
+		UserDTO dto = dao.findByUniqueKey("loginId", login);
+		return dto;
+	}
+	
+	public UserDTO authenticate(String loginId, String password) {
+
+		UserDTO dto = dao.findByUniqueKey("loginId", loginId);
+
+		if (dto != null) {
+			if (dto.getPassword().equals(password)) {
+				return dto;
+			}
+		}
+		return null;
+	}
+	
+	@Transactional(propagation = Propagation.REQUIRED)
+	public long save(UserDTO dto) {
+		Long id = dto.getId();
+		if (id != null && id > 0) {
+			update(dto);
+		} else {
+			id = add(dto);
+		}
+		return id;
+	}
 }
